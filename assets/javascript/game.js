5edfe91446593e02f4e72f44f5ced0353ce30fae
Lucy Julian <lucyjulian@Lucys-MacBook-Pro.local>
2018-03-14 13:14:46 -0400

    guesses remaining only decreasing once

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,12 +8,38 @@
     // var coconut = ["c", "o", "c", "o", "n", "u", "t"]
     // var dragonfruit = ["d", "r", "a", "g", "o", "n", "f", "r", "u", "i", "t"]
     // var banana = ["b", "a", "n", "a", "n", "a"]
+    
+    var letterAppearsInWord
+    var startingGuesses=10
+    
+    newGuessesRemaining = function(){
+
+        if (letterAppearsInWord === false){
+            guessesRemaining= startingGuesses - 1
+            
+        }
+        if (letterAppearsInWord === true){
+            guessesRemaining= startingGuesses
+        }
+            
+        
+        document.getElementById("remainingGuesses").innerHTML = guessesRemaining
+    }
+
+
+
+
 var wordOptions = ["papaya", "mango", "guava", "pineapple", "coconut", "dragonfruit", "banana"];
-var guessesRemaining = 10;
+var guessesRemaining = newGuessesRemaining();
 var lettersAlreadyGuessed = [ ];
 var chosenWord;
 var randomWordArray;
 var blanksArray = [];
+//var possibleGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
+
+
     
 
         chosenWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
@@ -21,6 +47,9 @@ var blanksArray = [];
         for (var i = 0; i < randomWordArray.length; i++){
             blanksArray[i] = ["_"];
         }
+
+
+        
         
 
     
@@ -34,14 +63,31 @@ var blanksArray = [];
       var userGuess = event.key;
 
       for (var i = 0; i < randomWordArray.length; i++){
-          if (userGuess === randomWordArray[i]){
-              blanksArray[i] = userGuess;
+            if (userGuess === randomWordArray[i]){
+                blanksArray[i] = userGuess;
 
-              document.getElementById("areaOfHTMLForWord").innerHTML = blanksArray.join(" ");
-              }
+                document.getElementById("areaOfHTMLForWord").innerHTML = blanksArray.join(" ");
+                letterAppearsInWord = true;
+                
+            }
+            
+            else (letterAppearsInWord=false);
+                
+            newGuessesRemaining();
+            }
+
+
+    // for (var i = 0; i < possibleGuesses.length; i++){
+    //     if (userGuess === possibleGuesses[i]){
+    //         possibleGuesses.
+    //     }
+    // }
+              
               
             
-          }
+            
+            
+            
       
         
 
@@ -58,5 +104,5 @@ var blanksArray = [];
         document.getElementById("pastGuessesGoHere").innerHTML=lettersAlreadyGuessed;
     
     
-    }
+        }
     ;
